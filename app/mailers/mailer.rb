a84425333d506bd62007ef8766440817982d1829
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-05-27 17:54:28 -0400

    Before sending an email, make sure we haven't sent one in the last 6 days.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,6 +2,8 @@ class Mailer < ActionMailer::Base
   default from: "from@example.com"
 
   def virtue(virtue, email)
+    SentEmail.create(:virtue_id => virtue.id, :email_id => email.id)
+
     mail(:to => email.address, :subject => "This week's virtue: #{virtue.name}", :body => virtue.content)
   end
 end
--- lib/jobs/send_email.rb
+++ lib/jobs/send_email.rb
@@ -6,6 +6,11 @@ module Jobs
 
       return unless email.present? && virtue.present?
 
+      # Be careful to avoid sending someone too many emails.
+      if email.sent_emails.where('created_at > ?', Time.now - 6.days).present?
+        raise "Tried to send another email too soon to email: #{email_id}, virtue: #{virtue_id}."
+      end
+
       Mailer.virtue(virtue, email).deliver
     end
   end
