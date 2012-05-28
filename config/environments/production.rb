1d0efe9603d6a0d6cf58762ed355ea6780b3976b
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-05-28 14:41:01 -0400

    Production email settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ Franklin::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'kirklander.info',
+    user_name: 'rob2@kirklander.info',
+    password: 'ciiMe0Oh',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
